d71f066c798acd451639d064632e22252988ba11
Snizhana-Tkachenko <snizhanatkacehko@gmail.com>
2023-08-22 11:30:37 +0200

    Add main file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import RandomNumberGenerator from './components/RandomNumberGenerator';
+import BubbleSorter from './components/BubbleSorter';
+import PyramidSorter from './components/PyramidSorter';
+import InsertionSorter from './components/InsertionSorter';
+import SelectionSorter from './components/SelectionSorter'; 
 
-function App() {
+const App = () => {
+  const [randomNumbers, setRandomNumbers] = useState([]);
+  const [selectedComponent, setSelectedComponent] = useState(null);
+
+
+  const handleGenerate = (numbers) => {
+    setRandomNumbers(numbers);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="RandomSortBlock">
+      <h2 className="RandomHeader">Random Number Generator</h2>
+      <RandomNumberGenerator onGenerate={handleGenerate} />
+      <div className="DroListContenier">
+      <select className="DropList" onChange={(e) => setSelectedComponent(e.target.value)} value={selectedComponent}>
+        <option value="">Choose Sorting Method </option>
+        <option value="bubble">Sort by Bubble</option>
+        <option value="heap">Sort by Heap</option>
+        <option value="insertion">Sort by Insertion</option>
+        <option value="selection">Sort by Selection</option>
+      </select>
+      </div>
+      {selectedComponent === 'bubble' && <BubbleSorter numbers={randomNumbers} />}
+      {selectedComponent === 'heap' && <PyramidSorter numbers={randomNumbers} />}
+      {selectedComponent === 'insertion' && <InsertionSorter numbers={randomNumbers} />}
+      {selectedComponent === 'selection' && <SelectionSorter numbers={randomNumbers} />}
     </div>
   );
-}
+};
 
 export default App;
